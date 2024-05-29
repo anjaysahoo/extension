ae5fbff829b3b62ef66afb398cbb80cdb1b9ed3d
Anjay Sahoo <65654701+anjaysahoo@users.noreply.github.com>
2024-05-29 08:59:32 +0530

    ➕feature: added full setup for developing an extension using svelte

---
+++ full-setup-svelte-ts-graphql-axios/README.md
@@ -0,0 +1,21 @@
+## Complete End-to-End Extension Setup
+
+### Tech Stack
+1. CXRJS
+2. Svelte
+3. TypeScript
+4. Graphql
+5. Codegen
+6. Axios
+
+
+### Steps
+
+1. Follow the setup used in `svelte-using-crxjs` project first
+2. Then follow the rest from here: https://github.com/anjaysahoo/basic-lens-protocol-nodejs-app
+
+All the folder structuring is same as LensView's `web-app`
+
+This is the same setup I have used for my app LensView app
+<br>
+https://github.com/BlockchainKami/lensview-beta
---
+++ full-setup-svelte-ts-graphql-axios/src/popup/Popup.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import getChallengeInfoLensService from "../services/lens/get-challenge-info.lens.service";
+  import searchPublicationAppService from "../services/app/search-publication.app.service";
+
+  onMount(() => {
+    test();
+
+    testlensviewAPI();
+  });
+
+  const test = async () => {
+    const challenge = await getChallengeInfoLensService(
+      "0xa03dbDA93D960BBDa97B9a2F74729de186C22638",
+      "0x018776"
+    );
+    console.log("challenge: ", challenge);
+  };
+
+  const testlensviewAPI = async () => {
+    const resp = await searchPublicationAppService("https://revoke.cash/");
+    console.log("resp : ", resp);
+  };
+</script>
+
+<main>
+  <h1>Popup</h1>
+</main>
+
+<style lang="scss">
+</style>
